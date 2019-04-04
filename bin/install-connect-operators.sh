2386d0bb000eeb49b30878d903082694bf5eba41
ennovatenow <anish.thomas@evolvussolutions.com>
2019-04-04 13:03:53 +0530

    add installers

---
+++ bin/install-connect-operators.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./conf/.project.env.sh
+source $PRECISION100_FOLDER/conf/.env.sh
+
+EXECUTION_NAME=$(cat "$PRECISION100_PROJECT_CONF_FOLDER/.execution.pid")
+source "$PRECISION100_PROJECT_CONF_FOLDER/$EXECUTION_NAME.env.sh"
+
+source $PRECISION100_FOLDER/conf/.execution.env.sh
+
+if [ ! -z "$1" ]; then
+    export OPERATION_MODE="PROD"
+fi
+if [ ! -z "$2" ]; then
+    export SIMULATION_MODE="TRUE"
+fi
+
+$PRECISION100_BIN_FOLDER/install-connect-operator.sh $1
+
