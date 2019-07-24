dcb42389347db8961671b9c10a2476213a30de1c
Anish Thomas <anish.thomas@evolvussolutions.com>
2019-07-24 13:25:12 +0530

    added validations to the scripts

--- bin/install-connect-operators.sh
+++ bin/install-connect-operators.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+function usage() {
+  echo "Usage: $0 <folder where the operators are downloaded> <operator name> "
+  echo "e.g. $0 /home/dell/precision100-operators sql"
+}
+
+if [[ ( "$#" -ne 2 ) ]]; then
+  usage
+  exit 1
+fi
+
+if [ ! -f ./conf/.project.env.sh ]; then
+   echo "Misconfigured installation - missing files in conf directory"
+   exit 10
+fi
 source ./conf/.project.env.sh
+
+if [ -z "$PRECISION100_FOLDER" ] || [ ! -f $PRECISION100_FOLDER/conf/.env.sh ]; then
+   echo "Misconfigured installation - Invalid Precision100 installation"
+   exit 10
+fi
 source $PRECISION100_FOLDER/conf/.env.sh
 
 $PRECISION100_BIN_FOLDER/install-connect-operator.sh $1 $2
