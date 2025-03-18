7dcc302466ef82e82328677919ec479c5186b748
ennovatenow <anish@paycorp.io>
2025-03-18 19:14:23 +0530

    configure auton installs

--- configure-project.sh
+++ configure-project.sh
@@ -66,6 +66,20 @@ function ask_project_name_qn() {
   PRECISION100_PROJECT_NAME="${INPUT_PROJECT_NAME:-$DEFAULT_PROJECT_NAME}"
 }
 
+function install_package() {
+  if [ "${1}" == "PYPI" ]; then
+     python3 -m pip -q install "$2"
+     return $?
+  fi
+  if [ "${1}" == "TESTPYPI" ]; then
+     python3 -m pip -q install --index-url https://test.pypi.org/simple "${2}" 
+     return $?
+  fi
+  package_name=$(echo $2 | tr -s '-' '_')
+  python3 -m pip -q install ${1}/${package_name}*
+  return $?
+}
+
 if [[ ( "$#" -gt 3 ) ]]; then
   usage
   exit 1
@@ -77,23 +91,102 @@ if ! command -v python3 &>/dev/null; then
   exit 1
 fi
 
-# Check if python3 version is at least 3.8
+# Check if python3 version is at least 3.11
 PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
-REQUIRED_VERSION="3.8"
+REQUIRED_VERSION="3.11"
 
 if [[ $(echo -e "$PYTHON_VERSION\n$REQUIRED_VERSION" | sort -V | head -n1) != "$REQUIRED_VERSION" ]]; then
-  echo "Python version 3.8 or above is required. Installed version is $PYTHON_VERSION."
+  echo "Python version 3.11 or above is required. Installed version is $PYTHON_VERSION."
   exit 1
 fi
 
+echo "Checking if virtual env has been created"
+if [[ ! -f "./dev/pyvenv.cfg" ]] ; then
+  echo "Virtual environment not created"
+  echo "Creating virtual environment"
+  python3 -m venv dev
+else
+  echo "virtual env created"
+fi
+source ./dev/bin/activate
 
+echo "Checking PRECISION_PACKAGE_REPOSITORY value"
+if [ -z "${PRECISION_PACKAGE_REPOSITORY}" ] ; then
+  echo "PRECISION_PACKAGE_REPOSITORY value not set defaulting to PYPI"
+  PRECISION_PACKAGE_REPOSITORY="PYPI"
+fi
+if [ "${PRECISION_PACKAGE_REPOSITORY}" != "PYPI" ] && [ "${PRECISION_PACKAGE_REPOSITORY}" != "TESTPYPI" ] && [ ! -d "${PRECISION_PACKAGE_REPOSITORY}" ] ; then
+  echo "Only PYPI, TESTPYPI and path to folder are supported values for PRECISION_PACKAGE_REPOSITORY"
+  exit 1
+fi 
+
+echo "Checking dependencies"
+
+echo "Checking Precision 100"
 # Check if precision_100 package is installed
 if ! python3 -c "import p100" &>/dev/null; then
-  echo "precision_100 package is not installed. Please install it to proceed."
-  exit 1
+  echo "precision_100 package is not installed. "
+  echo "attempting to install precision_100 package from ${PRECISION_PACKAGE_REPOSITORY}"
+  install_package "${PRECISION_PACKAGE_REPOSITORY}" "precision-100"
+  result="$?"
+  if [ "$result" -ne 0 ]; then
+     echo "Unable to install precision_100, exiting.."
+     exit $result;
+  fi
 fi
 
+echo "Checking Precision 100 Operators"
+# Check if precision_100_operators package is installed
+if ! python3 -c "import p100.operator.repo" &>/dev/null; then
+  echo "precision_100_operator package is not installed. "
+  echo "attempting to install precision_100 package from ${PRECISION_PACKAGE_REPOSITORY}"
+  install_package "${PRECISION_PACKAGE_REPOSITORY}" "precision-100-operators"
+  result="$?"
+  if [ "$result" -ne 0 ]; then
+     echo "Unable to install precision_100_operators, exiting.."
+     exit $result;
+  fi
+fi
+
+echo "Checking cryptography"
+# Check if cryptography package is installed
+if ! python3 -c "from cryptography.fernet import Fernet" &>/dev/null; then
+  echo "cryptography package is not installed. You will not be able to encrypt secrets".
+  echo "Recommeded to install the package and encrypt all secrets"
+  echo "attempting to install cryptography package from ${PRECISION_PACKAGE_REPOSITORY}"
+  install_package "${PRECISION_PACKAGE_REPOSITORY}" "cryptography"
+  result="$?"
+  if [ "$result" -ne 0 ]; then
+     echo "Unable to install cryptography, exiting.."
+     echo "cryptography package is not installed. You will not be able to encrypt secrets".
+  fi
+fi
+
+#Check if orcl package is installed
+if ! python3 -c "import p100.operator.orcl" &>/dev/null; then
+  echo "orcl package is not installed."
+  echo "attempting to install orcl package from ${PRECISION_PACKAGE_REPOSITORY}"
+  install_package "${PRECISION_PACKAGE_REPOSITORY}" "precision-100-orcl-operators"
+  result="$?"
+  if [ "$result" -ne 0 ]; then
+     echo "Unable to install precision_100_orcl_operators, exiting.."
+     exit $result;
+  fi
+fi
+
+echo "Dependecy check completed."
+
 PRECISION100_VERSION=$(python -c "from importlib.metadata import version;  print(version('precision-100'))")
+echo "Found Precision 100 $PRECISION100_VERSION version"
+
+PRECISION100_OPERATOR_VERSION=$(python -c "from importlib.metadata import version;  print(version('precision-100-operators'))")
+echo "Found Precision 100 Operator $PRECISION100_OPERATOR_VERSION version"
+
+CRYPTOGRAPHY_VERSION=$(python -c "from importlib.metadata import version;  print(version('cryptography'))")
+echo "Found cryptography $CRYPTOGRAPHY_VERSION version"
+
+PRECISION100_ORCL_OPERATOR_VERSION=$(python -c "from importlib.metadata import version;  print(version('precision-100-orcl-operators'))")
+echo "Found Precision 100 Orcle Operator $PRECISION100_ORCL_OPERATOR_VERSION version"
 
 banner
 PRECISION100_FOLDER=$(pwd)/precision100
